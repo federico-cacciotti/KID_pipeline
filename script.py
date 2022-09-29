16b2ce992146a26dc1135dd133c235068a3038a0
Federico <fe.cacciotti@gmail.com>
2022-09-29 11:03:33 +0200

    Update script.py

--- script.py
+++ script.py
@@ -12,9 +12,9 @@ peaks, peaks_info = v140.findPeaks(mag_filt='airp_lss',
 
 
 v270 = pl.VNA.VNA(filename='20220531_04', temperature=270)
-#v270.plotVNA()
+v270.plotVNA()
 
-#peaks, peaks_info = v270.findPeaks(peak_width=(1.0, 150.0), peak_height=1.0, peak_prominence=(1.0, 30.0))
+peaks, peaks_info = v270.findPeaks(peak_width=(1.0, 150.0), peak_height=1.0, peak_prominence=(1.0, 30.0))
 
 #v270_target = v270.extractTarget(peaks, peaks_info)
 #v270.fitS21(v270_target, channel='all')
